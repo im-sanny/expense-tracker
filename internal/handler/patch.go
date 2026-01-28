e7166080392bfedb95baf0239c07a7c9ab57eede
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2026-01-28 18:42:39 +0600

    refactor(handler): implement proper error handling in PATCH

--- internal/handler/patch.go
+++ internal/handler/patch.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"database/sql"
 	"encoding/json"
 	"expense-tracker/internal/model"
 	"net/http"
@@ -13,27 +14,38 @@ func (h *Handler) Patch(w http.ResponseWriter, r *http.Request) {
 	idStr := r.PathValue("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		http.Error(w, "Invalid id", http.StatusBadRequest)
+		http.Error(w, "invalid expense id", http.StatusBadRequest)
 		return
 	}
 
 	var e model.Expense
-	err1 := json.NewDecoder(r.Body).Decode(&e)
-	if err1 != nil {
-		http.Error(w, "Failed to decode body", http.StatusInternalServerError)
+	err = json.NewDecoder(r.Body).Decode(&e)
+	if err != nil {
+		http.Error(w, "invalid JSON body", http.StatusBadRequest)
 		return
 	}
 
-	err2 := h.DB.QueryRow(`UPDATE expenses SET amount = COALESCE($1, amount), note = COALESCE($2, note) WHERE id=$3 RETURNING id, date, amount, note`, e.Amount, e.Note, id).Scan(&e.ID, &e.Date, &e.Amount, &e.Note)
+	err = h.DB.QueryRow(`
+	UPDATE expenses SET
+	amount = COALESCE($1, amount),
+	note = COALESCE($2, note)
+	WHERE id=$3 RETURNING id, date, amount, note`,
+		e.Amount, e.Note, id).Scan(
+		&e.ID, &e.Date, &e.Amount, &e.Note)
 
-	if err2 != nil {
-		http.Error(w, "Invalid query", http.StatusInternalServerError)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "expense not found", http.StatusNotFound)
+			return
+		}
+
+		http.Error(w, "failed to update expense", http.StatusInternalServerError)
 		return
 	}
 
-	err3 := json.NewEncoder(w).Encode(e)
-	if err3 != nil {
-		http.Error(w, "Failed encode response", http.StatusInternalServerError)
+	err = json.NewEncoder(w).Encode(e)
+	if err != nil {
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
 		return
 	}
 }
