9192bd4f21e98e27deb254d320f4480a5c9b29d2
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2026-01-28 20:04:43 +0600

    refactor(handler): implement proper error handling in POST

--- internal/handler/post.go
+++ internal/handler/post.go
@@ -11,27 +11,27 @@ func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
 	var e model.Expense
 	err := json.NewDecoder(r.Body).Decode(&e)
 	if err != nil {
-		http.Error(w, "Failed to decode the body", http.StatusBadRequest)
+		http.Error(w, "invalid JSON body", http.StatusBadRequest)
 		return
 	}
 
 	e.Date = time.Now()
-	err1 := h.DB.QueryRow(`
+	err = h.DB.QueryRow(`
 	INSERT INTO
 	expenses(date, amount, note)
 	VALUES($1, $2, $3)
 	RETURNING id, date, amount, note`,
-		e.Date, e.Amount, e.Note).Scan(&e.ID,
-		&e.Date, &e.Amount, &e.Note)
+		e.Date, e.Amount, e.Note).Scan(
+		&e.ID, &e.Date, &e.Amount, &e.Note)
 
-	if err1 != nil {
-		http.Error(w, "Error in query", http.StatusInternalServerError)
+	if err != nil {
+		http.Error(w, "failed to insert data in expense", http.StatusInternalServerError)
 		return
 	}
 
-	err2 := json.NewEncoder(w).Encode(e)
-	if err2 != nil {
-		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+	err = json.NewEncoder(w).Encode(e)
+	if err != nil {
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
 		return
 	}
 }
