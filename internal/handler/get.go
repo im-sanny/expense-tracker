40673f2006e40224c500c0bc135a987f394017c1
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2026-01-28 21:11:53 +0600

    refactor(handler): implement proper error handling in GET by id

--- internal/handler/get.go
+++ internal/handler/get.go
@@ -4,32 +4,38 @@ import (
 	"encoding/json"
 
 	"expense-tracker/internal/model"
-	"log"
 	"net/http"
 )
 
 func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var et []model.Expense
 
 	rows, err := h.DB.Query(`SELECT id, date, amount, note FROM expenses`)
 	if err != nil {
-		http.Error(w, "Failed to query expense", http.StatusInternalServerError)
+		http.Error(w, "failed to get expense", http.StatusInternalServerError)
 		return
 	}
 
+	defer rows.Close()
+
+	var expense []model.Expense
 	for rows.Next() {
 		var e model.Expense
 		err := rows.Scan(&e.ID, &e.Date, &e.Amount, &e.Note)
 		if err != nil {
-			http.Error(w, "Failed to scan row", http.StatusInternalServerError)
+			http.Error(w, "failed to read expense data", http.StatusInternalServerError)
 			return
 		}
-		et = append(et, e)
+		expense = append(expense, e)
 	}
 
-	err1 := json.NewEncoder(w).Encode(et)
-	if err != nil {
-		log.Println(err1)
+	if err = rows.Err(); err != nil {
+		http.Error(w, "incomplete expense data", http.StatusInternalServerError)
+		return
+	}
+
+	if err = json.NewEncoder(w).Encode(expense); err != nil {
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+		return
 	}
 }
--- internal/handler/getById.go
+++ internal/handler/getById.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"database/sql"
 	"encoding/json"
 	"expense-tracker/internal/model"
 
@@ -14,23 +15,30 @@ func (h *Handler) GetById(w http.ResponseWriter, r *http.Request) {
 	idStr := r.PathValue("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		http.Error(w, "Invalid id", http.StatusBadRequest)
+		http.Error(w, "invalid expense id", http.StatusBadRequest)
 		return
 	}
 
 	var e model.Expense
 
-	err1 := h.DB.QueryRow(`
+	err = h.DB.QueryRow(`
 	SELECT id, date, amount, note
 	FROM expenses
-	WHERE id=$1`, id).Scan(&e.ID, &e.Date, &e.Amount, &e.Note)
-	if err1 != nil {
-		http.Error(w, "Failed to query", http.StatusInternalServerError)
+	WHERE id=$1`, id).Scan(
+		&e.ID, &e.Date, &e.Amount, &e.Note)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "expense not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "failed to get expense", http.StatusInternalServerError)
 		return
 	}
 
-	err2 := json.NewEncoder(w).Encode(e)
-	if err2 != nil {
-		http.Error(w, "Failed to encode the response", http.StatusInternalServerError)
+	err = json.NewEncoder(w).Encode(e)
+	if err != nil {
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+		return
 	}
 }
