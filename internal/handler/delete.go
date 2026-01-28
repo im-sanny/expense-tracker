104a34837d974fa60eb5f530abee45a8b57e467f
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2026-01-28 21:52:16 +0600

    refactor(handler): implement proper error handling in DELETE

--- internal/handler/delete.go
+++ internal/handler/delete.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -12,13 +11,13 @@ func (h *Handler) Delete(w http.ResponseWriter, r *http.Request) {
 	idStr := r.PathValue("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		http.Error(w, "Invalid id", http.StatusBadRequest)
+		http.Error(w, "invalid expense id", http.StatusBadRequest)
 		return
 	}
 
 	rows, err := h.DB.Exec(`DELETE FROM expenses WHERE id=$1`, id)
 	if err != nil {
-		log.Println(err)
+		http.Error(w, "failed to delete expense", http.StatusInternalServerError)
 		return
 	}
 
@@ -33,4 +32,5 @@ func (h *Handler) Delete(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.WriteHeader(http.StatusNoContent)
 }
