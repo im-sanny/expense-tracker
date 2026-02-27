8e1159189e7e9329256dfd6eb43918531febb601
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2026-02-27 21:26:55 +0600

    feat(middleware): introduce recover middleware

--- internal/middlewares/middleares.go
+++ internal/middlewares/middleares.go
@@ -4,6 +4,17 @@ import (
 	"net/http"
 )
 
+func Recover(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				http.Error(w, "internal server error", 500)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
 func JSONMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
